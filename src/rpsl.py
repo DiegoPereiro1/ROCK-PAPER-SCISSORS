00f78ee3c35440ecfd38d75c94c426eb1cd79c90
Diego Pereiro <diegopereiromartinezsayanes@gmail.com>
2024-01-12 18:00:38 +0100

    Adaptada la lógica de get_computer_action a RPSL

--- src/rpsl.py
+++ src/rpsl.py
@@ -19,11 +19,11 @@ class GameResult(IntEnum):
 
 Victories = {
     
-    GameAction.Rock: [GameAction.Paper,GameAction.Lizard],
+    GameAction.Rock: [GameAction.Lizard,GameAction.Scissors],
     
-    GameAction.Paper: [GameAction.Scissors,GameAction.Spock],
+    GameAction.Paper: [GameAction.Rock,GameAction.Spock],
     
-    GameAction.Scissors: [GameAction.Rock,GameAction.Lizard],
+    GameAction.Scissors: [GameAction.Lizard,GameAction.Paper],
     
     GameAction.Lizard: [GameAction.Paper,GameAction.Spock],
     
@@ -32,39 +32,6 @@ Victories = {
     
     
 }
-
-def assess_game_silent(user_action, computer_action):
-
-    game_result = None
-
-    if user_action == computer_action:
-        game_result = GameResult.Tie
-
-
-    elif user_action in (Victories[computer_action]):
-        game_result = GameResult.Victory
-
-   
-    else:          
-        game_result = GameResult.Defeat
-    
-
-    return game_result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -75,9 +42,7 @@ class EstrategiaRandom:
         computer_action = GameAction(computer_selection)
         print(f"Computer picked {computer_action.name}.")    
         return computer_action
-    
-    
-    
+
 class EstrategiaPrincipal:
     def __init__(self):
         self.history=[]
@@ -88,13 +53,13 @@ class EstrategiaPrincipal:
         if len(self.history)==0:#Comportamiento en la primera ronda
             computer_selection = random.randint(0, len(GameAction) - 1)
             computer_action = GameAction(computer_selection)
-        
-       
+
         elif assess_game_silent(self.history[-1][0],self.history[-1][1])==GameResult.Defeat:#Cuando la máquina pierde en la ultima ronda
             past_user_action=self.history[-1][0]
             if len(self.history)>=2 and assess_game_silent(self.history[-2][0],self.history[-2][1])==GameResult.Defeat:#Esto si ha perdido dos veces seguidas
                 
-                lista_posibilidades=[GameAction.Rock,GameAction.Scissors,GameAction.Paper]
+                lista_posibilidades=list(Victories.keys()) # TODO: Que la lista de posibilidades se genere de forma dinamica utilizando las claves de Victories
+                
                 past_computer_action=self.history[-1][1]
                 past_past_computer_action=self.history[-2][1]
                 
@@ -104,15 +69,20 @@ class EstrategiaPrincipal:
                     lista_posibilidades.remove(past_computer_action)#Se elimina el lo que se ha utilizado en el penúltimo turno
                     
                 computer_selection = random.randint(0, len(lista_posibilidades) - 1)
-                computer_action = GameAction(computer_selection)  
+                computer_action = lista_posibilidades[computer_selection]
                       
             else:#Cuando solo ha perdido una vez    
-                if past_user_action==GameAction.Scissors:
-                    computer_action=GameAction.Rock
-                elif past_user_action==GameAction.Rock:
-                    computer_action=GameAction.Paper
-                else: 
-                    computer_action=GameAction.Scissors
+                user_action in (Victories[computer_action])
+                lista_acciones=list(Victories.keys())
+                lista_acciones_derrota=Victories[past_user_action]
+                for accion in lista_acciones_derrota:
+                    try:
+                        lista_acciones.remove(accion)
+                    except ValueError:
+                        pass
+                
+                computer_selection = random.randint(0, len(lista_acciones) - 1)    
+                computer_action = lista_acciones[computer_selection]        
             
         elif assess_game_silent(self.history[-1][0],self.history[-1][1])==GameResult.Victory: # Esoto es si ha ganado la última ronda
             past_user_action=self.history[-1][0]
@@ -123,38 +93,31 @@ class EstrategiaPrincipal:
                 computer_action = past_user_action
         
         elif assess_game_silent(self.history[-1][0],self.history[-1][1])==GameResult.Tie: # Esto es si ha empatado la ultima ronda
-            lista_posibilidades=[GameAction.Rock,GameAction.Scissors,GameAction.Paper]
+            lista_posibilidades=list(Victories.keys())
             past_computer_action=self.history[-1][1]
             if past_computer_action in lista_posibilidades:
                 lista_posibilidades.remove(past_computer_action)#Se elimina el lo que se ha utilizado en el último turno
             computer_selection = random.randint(0, len(lista_posibilidades) - 1)
-            computer_action = GameAction(computer_selection) 
+            computer_action = lista_posibilidades[computer_selection] 
                             
         self.history.append([user_action,computer_action])
         print(f"Computer picked {computer_action.name}.") 
         return computer_action 
-    
 
 def assess_game_silent(user_action, computer_action):
-
+    # Esta función indica si gana el agente o no 
     game_result = None
 
     if user_action == computer_action:
         game_result = GameResult.Tie
 
-
     elif user_action in (Victories[computer_action]):
         game_result = GameResult.Victory
 
-   
     else:          
         game_result = GameResult.Defeat
-    
 
     return game_result
-    
-   
-
 
 def assess_game(user_action, computer_action):
 
@@ -176,7 +139,6 @@ def assess_game(user_action, computer_action):
 
    
 
-
 def get_computer_action():
     computer_selection = random.randint(0, len(GameAction) - 1)
     computer_action = GameAction(computer_selection)
@@ -191,8 +153,6 @@ def get_user_action():
     game_choices_str = ", ".join(game_choices)
     user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
     user_action = GameAction(user_selection)
-    
-    
     
     return user_action
 
